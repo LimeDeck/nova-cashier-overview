6ed7bffa7e6b0b2f9616f807d7a7a5e7710c80b5
Rigel Kent Carbonel <rigel20.kent@gmail.com>
2022-04-10 22:49:32 +0800

    Update for laravel nova 4

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,10 @@
-let mix = require('laravel-mix')
+let mix = require('laravel-mix');
+
+require('./mix');
 
 mix
   .setPublicPath('dist')
   .js('resources/js/tool.js', 'js')
-  .vue({ version: 2 })
+  .vue({ version: 3 })
+  .css('resources/css/tool.css', 'css')
+  .nova('limedeck/nova-cashier-overview');
